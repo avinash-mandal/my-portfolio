22219ab348a1c362e02b39fc5b89f02a7c910e25
avinash-mandal <avinashhm012@gmail.com>
2026-04-17 13:40:48 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Message sent successfully!");
+});
